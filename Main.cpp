8a313f99c6533f09a0ff7348a54069fd9ddc9cfe
hopedantilope <84099562+hopedantilope@users.noreply.github.com>
2021-12-15 16:25:46 +0100

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,3 @@
-#include <DNSServer.h>
-
 #include <analogWrite.h>
 #include "UbidotsEsp32Mqtt.h"
 #include <TinyGPS++.h>
@@ -9,8 +7,9 @@
 
 RTC_DATA_ATTR int bootCount = 0;
 
-RTC_DATA_ATTR int Critical_Value = 10;
-RTC_DATA_ATTR int Current_Value = 5;
+RTC_DATA_ATTR int Critical_Value = 5;
+RTC_DATA_ATTR float maxWaterLevel = 12;
+RTC_DATA_ATTR float Current_Value;
 RTC_DATA_ATTR int TIME_TO_SLEEP = 10;
 
 RTC_DATA_ATTR float prevAvg;
@@ -24,12 +23,12 @@ const char *WIFI_PASS = "123454321";      // Put here your Wi-Fi password
 const char *DEVICE_LABEL = "ESP32";
 const char *VARIABLE_LABEL1 = "Vann.mgd & Pos."; // Put here your Variable label to which data  will be published
 
-char* str_lat = (char*)malloc(sizeof(char) * 10);
-char* str_lng = (char*)malloc(sizeof(char) * 10);
-char* context = (char*)malloc(sizeof(char) * 30);
+RTC_DATA_ATTR char* str_lat = (char*)malloc(sizeof(char) * 10);
+RTC_DATA_ATTR char* str_lng = (char*)malloc(sizeof(char) * 10);
+RTC_DATA_ATTR char* context = (char*)malloc(sizeof(char) * 30);
 
-RTC_DATA_ATTR float lat;
-RTC_DATA_ATTR float lng;
+RTC_DATA_ATTR float lat; //Placeholder
+RTC_DATA_ATTR float lng; //Placeholder
 
 static const int RXPin = 26, TXPin = 25;
 static const uint32_t GPSBaud = 9600;
@@ -53,75 +52,6 @@ Ubidots ubidots(UBIDOTS_TOKEN);
 #define sequence 5  //fem målinger blir tatt
 #define totalHeight 30 
 
-void setup(){
-  Serial.begin(9600);
-  ss.begin(GPSBaud);
-  
-  delay(1000); //Take some time to open up the Serial Monitor
-
-  //Increment boot number and print it every reboot
-  ++bootCount;
-  Serial.println(lat);
-  Serial.println(lng);
-  Serial.println("Boot number: " + String(bootCount));
-  Serial.print("Distance is ");
-  Serial.println(returnAverage());
-
-  Sleep();
-  
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_MIN_FACTOR);
-  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
-  " Minutes");
-
-
-  delay(5000);
-  gps.encode(ss.read());
-    if (gps.location.isUpdated()){
-      Serial.println("GPS UPDATEEEEER");
-      lat = gps.location.lat();
-      lng = gps.location.lng();
-    }
- Serial.println(gps.location.lat());
-  ubidots.setDebug(true);  // uncomment this to make debug messages available
-  ubidots.connectToWifi(WIFI_SSID, WIFI_PASS);
-  ubidots.setCallback(callback);
-  ubidots.setup();
-  ubidots.reconnect();
-
-  timer = millis();
-
-  sprintf(str_lat, "%f", lat);
-  sprintf(str_lng, "%f", lng);
-
-  ubidots.addContext("lat", str_lat);
-  ubidots.addContext("lng", str_lng);
-  ubidots.getContext(context);
-
-  if (!ubidots.connected())
-  {
-    ubidots.reconnect();
-  }
-  ubidots.add(VARIABLE_LABEL1, average, context); // Insert your variable Labels and the value to be sent
- // ubidots.add(VARIABLE_LABEL2, 2, context);
-  ubidots.publish(DEVICE_LABEL);
-  
-  //Go to sleep now
-  //esp_deep_sleep_start();
-}
-
-void Sleep(){
-  if (Critical_Value < Current_Value){
-    TIME_TO_SLEEP = 1;
-    
-  }
-  else if (Current_Value/Critical_Value>1.1){
-    TIME_TO_SLEEP = 5;
-  }
-  else {
-    TIME_TO_SLEEP = 10;
-  }
-}
-
 float readingFlow ()
 {
   pinMode(ultraPIN, OUTPUT); //definerer den som en output
@@ -143,7 +73,7 @@ float returnAverage() //gjennomsnittsfunksjon
       sum = sum + (0.01723 * readingFlow());//Ganger med 0.1723 for å gjøre om til millimeter
       delay(50); //Pause
       if (i == (sequence - 1)){
-        average = sum / sequence;
+        average = maxWaterLevel - (sum / sequence);
         if (counter == 1 && abs(average - prevAvg) < discardValue){ //vannnivå og IKKE avstand fra sensor til vann
           return average;
         }
@@ -154,6 +84,18 @@ float returnAverage() //gjennomsnittsfunksjon
       }
     }
 } 
+
+void Sleep(){
+  if (Critical_Value < Current_Value){
+    TIME_TO_SLEEP = 5;
+  }
+  else if (Current_Value/Critical_Value>1.1){
+    TIME_TO_SLEEP = 10;
+  }
+  else {
+    TIME_TO_SLEEP = 30;
+  }
+}
 
 void callback(char *topic, byte *payload, unsigned int length)
 {
@@ -167,7 +109,51 @@ void callback(char *topic, byte *payload, unsigned int length)
   Serial.println();
 }
 
+void MQTT(){
+  ubidots.setDebug(true);  // uncomment this to make debug messages available
+  ubidots.connectToWifi(WIFI_SSID, WIFI_PASS);
+  ubidots.setCallback(callback);
+  ubidots.setup();
+  ubidots.reconnect();
+
+  timer = millis();
+  sprintf(str_lat, "%f", lat);
+  sprintf(str_lng, "%f", lng);
+
+  ubidots.addContext("lat", str_lat);
+  ubidots.addContext("lng", str_lng);
+  ubidots.getContext(context);
+
+  if (!ubidots.connected())
+  {
+    ubidots.reconnect();
+  }
+  ubidots.add(VARIABLE_LABEL1, average, context); // Insert your variable Labels and the value to be sent
+ // ubidots.add(VARIABLE_LABEL2, 2, context);
+  ubidots.publish(DEVICE_LABEL);
+  esp_deep_sleep_start();
+}
+
+
+void setup(){
+  Serial.begin(9600);
+  ss.begin(GPSBaud);
+  delay(1000); //Take some time to open up the Serial Monitor
+
+  //Increment boot number and print it every reboot
+  ++bootCount;
+  Serial.println("Boot number: " + String(bootCount));
+  Current_Value = returnAverage();
+  Serial.print("Distance is ");
+  Serial.println(Current_Value);
+  Sleep();
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_MIN_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
+  " Minutes");
+}
+
 void loop(){
+  if (bootCount==1){
   while (ss.available() > 0){
     gps.encode(ss.read());
     if (gps.location.isUpdated()){
@@ -177,7 +163,11 @@ void loop(){
       Serial.println(gps.location.lng(), 6);
       lng = gps.location.lng();
       lat = gps.location.lat();
-      esp_deep_sleep_start();
+      MQTT();
     }
+  }
+  }
+  if(bootCount!=1){
+    MQTT();
   }
 }
